d78567f97ac8f37d8bda90f24782195d6aed39f6
dyedfox <slavko7@gmail.com>
2022-06-28 17:29:00 +0300

    Synchonized

--- Lesson_5/find_dir.sh
+++ Lesson_5/find_dir.sh
@@ -1,12 +1,25 @@
 #!/bin/bash 
 
+#МЕТОД 1
+
 dir=0
 
 #всі вкладені директорії, для глибини пошуку можна застосувати аргумент -maxdepth 5, наприклад
 for file in $(find /etc -type d)
 do
-    echo "$file" >> etc_dir.txt.txt
+    echo $file >> etc_dir.txt
     ((dir++))
 
 done
 echo "Знайдено директорій: $dir"
+
+
+#МЕТОД 2, глибину пошуку можна регулювати зірочкою /*
+
+for file in /etc/*
+do
+if [ -d "$file" ] 
+then
+echo $file >> etc_dir2.txt
+fi
+done
